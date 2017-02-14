eadf543533def551ea6cb3b8ed38a0db76931437
Gavril <gavril.tonev@gmail.com>
2017-02-14 17:58:20 +0200

    Formatted the example project url's

--- SwiftImageCarouselExampleProject/SwiftImageCarouselExampleProject/ExampleViewController.swift
+++ SwiftImageCarouselExampleProject/SwiftImageCarouselExampleProject/ExampleViewController.swift
@@ -11,18 +11,37 @@ import UIKit
 
 class ExampleViewController: UIViewController {
     
-    var vc = UIViewController()
 
     @IBAction func touch(_ sender: UIBarButtonItem) {
-        (vc as! SwiftImageCarouselVC).contentImageURLs = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEk1nFxHenk7Mjlu0j25zlRobxzos707fsGuruD5ZDlHSzaAA7", "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTxbyrtvNkjkgJ2a3KxOjNSq5-T5233hxtN86B2QD3NUaPFid4Vyg", "https://s-media-cache-ak0.pinimg.com/originals/c0/6d/b9/c06db90191ec07e7c7d24a99cf34afd3.jpg", "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTxbyrtvNkjkgJ2a3KxOjNSq5-T5233hxtN86B2QD3NUaPFid4Vyg", "https://s-media-cache-ak0.pinimg.com/originals/c0/6d/b9/c06db90191ec07e7c7d24a99cf34afd3.jpg"]
-        
+//        (vc as! SwiftImageCarouselVC).contentImageURLs = [
+//            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEk1nFxHenk7Mjlu0j25zlRobxzos707fsGuruD5ZDlHSzaAA7",
+//            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTxbyrtvNkjkgJ2a3KxOjNSq5-T5233hxtN86B2QD3NUaPFid4Vyg",
+//            "https://s-media-cache-ak0.pinimg.com/originals/c0/6d/b9/c06db90191ec07e7c7d24a99cf34afd3.jpg",
+//            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTxbyrtvNkjkgJ2a3KxOjNSq5-T5233hxtN86B2QD3NUaPFid4Vyg",
+//            "https://s-media-cache-ak0.pinimg.com/originals/c0/6d/b9/c06db90191ec07e7c7d24a99cf34afd3.jpg"
+//        ]
     }
+    
     @IBOutlet var containerView: UIView!
-    var imageURLs = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEk1nFxHenk7Mjlu0j25zlRobxzos707fsGuruD5ZDlHSzaAA7", "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTxbyrtvNkjkgJ2a3KxOjNSq5-T5233hxtN86B2QD3NUaPFid4Vyg", "https://s-media-cache-ak0.pinimg.com/originals/c0/6d/b9/c06db90191ec07e7c7d24a99cf34afd3.jpg"]
+    var imageURLs = [
+        "https://store.storeimages.cdn-apple.com/4974/as-images.apple.com/is/image/AppleInc/aos/published/images/M/AC/MACBOOKPRO/MACBOOKPRO?wid=1200&hei=630&fmt=jpeg&qlt=95&op_sharpen=0&resMode=bicub&op_usm=0.5,0.5,0,0&iccEmbed=0&layer=comp&.v=6xyk93",
+        "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTxbyrtvNkjkgJ2a3KxOjNSq5-T5233hxtN86B2QD3NUaPFid4Vyg",
+        "https://s-media-cache-ak0.pinimg.com/originals/c0/6d/b9/c06db90191ec07e7c7d24a99cf34afd3.jpg"
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        vc = SwiftImageCarouselVC.instantiate(containerView: containerView, contentImageURLs: imageURLs, parentVC: self, swiftImageCarouselVCDelegate: self ) 
+        
+        let storyboard = UIStoryboard (name: "Main", bundle: Bundle(for: SwiftImageCarouselVC.self))
+        let vc = storyboard.instantiateInitialViewController() as! SwiftImageCarouselVC
+        vc.contentImageURLs = imageURLs
+        
+        // Adding it to the container view
+        vc.willMove(toParentViewController: self)
+        containerView.addSubview(vc.view)
+        vc.view.frame = CGRect(x: 0, y: 0, width: containerView.frame.width, height: containerView.frame.height)
+        self.addChildViewController(vc)
+        vc.didMove(toParentViewController: self)
     }
     
 }
